ca102a43277c8f1e8b2c41371fe97de6c1bbff48
s1f10210276 <s1F102102762@iniad.org>
2024-04-30 14:37:36 +0900

    Update 4.30

--- hungry/static/script.js
+++ hungry/static/script.js
@@ -1,9 +1,11 @@
-document.getElementById('fileInput').addEventListener('change', function () {
-    var icon = document.getElementById('uploadIcon');
-    if (icon) {
-        icon.outerHTML = '<span class="spinner-border"></span>';
-        document.getElementById("fileButton").disabled = true;
-        document.getElementById("fileButton").className = "btn btn-secondary";
-        document.getElementById("fileButton").innerHTML = "判定中";
-    }
+$(document).ready(function() {
+    $('#fileInput').on('change', function() {
+        var filename = $(this).val().split('\\').pop();
+        $('#filenameDisplay').val(filename);
+
+        document.getElementById('uploadIcon').outerHTML = '<span class="spinner-border"></span>';
+        document.getElementById('fileButton').disabled = true;
+        document.getElementById('fileButton').className = "btn btn-secondary";
+        document.getElementById('fileButton').innerHTML = "判定中";
+    });
 });
